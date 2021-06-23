12c6014d5007ff4c70cd644ced0105aab57511e6
AlessioEng <86313667+AlessioEng@users.noreply.github.com>
2021-06-23 10:13:40 +0200

    Add files via upload
    microservizio 2

---
+++ Microservizio2/src/main/java/Prova.java
@@ -0,0 +1,11 @@
+
+public class Prova {
+
+	public void HelloWorld() {
+		
+		System.out.println("Hello, World!");
+		boolean esito=true;
+		if(esito==true)
+			System.out.println("Hello, World!");
+	}
+}
